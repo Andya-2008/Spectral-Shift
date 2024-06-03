0fc255095691fcc1b49d5d42fe1c9deb96d47e45
ANDREW-LAPTOP\andre <andrewhyun@live.com>
2024-06-03 09:15:10 -0700

    New starting method for networking: Still need to test

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,6 +6,7 @@ using Unity.Netcode;
 public class GameManager : NetworkBehaviour
 {
     [SerializeField] GameObject Player;
+    bool hasSpawned = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,17 @@ public class GameManager : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.K) && !hasSpawned)
+        {
+            hasSpawned = true;
+            SpawnPlayer();
+        }
     }
 
     public void SpawnPlayer()
     {
         Instantiate(Player, new Vector3(0,0,0), Quaternion.identity);
         Player.GetComponent<NetworkObject>().SpawnWithOwnership(NetworkObjectId);
+        Camera.main.gameObject.SetActive(false);
     }
 }
